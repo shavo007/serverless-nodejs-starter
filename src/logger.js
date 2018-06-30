082eb78954776cc8afba076cbacdd63314fa6e41
shane lee <shanelee007@gmail.com>
2018-06-30 15:13:55 +1000

    update logger - winston major upgrade

--- src/logger.js
+++ src/logger.js
@@ -3,16 +3,16 @@ import winston from 'winston'
 winston.level = process.env.LOG_LEVEL || 'info'
 const handleExceptions = process.env.NODE_ENV !== 'dev'
 
-const logger = new winston.Logger({
-	level: winston.level,
-	transports: [
-		new winston.transports.Console({
-			handleExceptions,
-			humanReadableUnhandledException: handleExceptions,
-			level: 'debug'
-		})
-	],
-	exitOnError: false
+const logger = winston.createLogger({
+    level: winston.level,
+    transports: [
+        new winston.transports.Console({
+            handleExceptions,
+            humanReadableUnhandledException: handleExceptions,
+            level: 'debug'
+        })
+    ],
+    exitOnError: false
 })
 
 export default logger
